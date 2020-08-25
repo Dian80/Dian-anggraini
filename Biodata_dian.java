dc05a784b396b027e1d2d2da3a7c6c185435abda
Dian80 <70132205+Dian80@users.noreply.github.com>
2020-08-25 10:50:39 +0700

    Add files via upload

---
+++ Biodata_dian.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata_dian;
+
+/**
+ *
+ * @author ENDANG
+ */
+public class Biodata_dian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama="Dian Anggraini";
+        String Jk="Perempuan";
+        String Alamat="Jl.panditorejo tumpuk";
+        String Jurusan="XI RPL 1";
+        
+        System.out.println("biodata saya");//mengeprint atau mencetak
+        System.out.println("--------");
+        System.out.println("Nama \t\t\t:"+nama);
+        System.out.println("Jk \t\t\t:"+Jk);
+        System.out.println("Alamat \t\t\t:"+Alamat);
+        System.out.println("Jurusan \t\t\t:"+Jurusan);
+    }
+}
